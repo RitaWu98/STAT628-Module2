a8037e4cc32d58d9d03789e36451e7e0e0930c24
hpan55 <55922062+hpan55@users.noreply.github.com>
2019-10-07 11:53:20 -0500

    add mail address, notes and buttoms to app

--- code/bodyfatapp.R
+++ code/bodyfatapp.R
@@ -8,28 +8,33 @@
 #
 
 library(shiny)
-?fluidPage
+
 # Define UI for application that draws a histogram
-ui <- fluidPage(theme = "cerulean",
-    
+ui <- fluidPage(
+
     # Application title
-    titlePanel("Calculate Your BodyFat Percentage"),
-    
+    titlePanel("Calculate your BodyFat!"),
+
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
             numericInput("ABDOMEN",
-                         "Please input your abdomen (cm):",
-                         value = 90, step = 0.1,min=0),
-            helpText("Recommended Input Range: 65~130 (cm)"),
-            numericInput("WEIGHT", "Please input your weight (lbs):", value = 170,
-                         step = 0.25,min=0),
-            helpText("Recommended Input Range: 115~265 (lbs)")
+                        "Please input your abdomen(cm):",
+                        value = 90, step = 0.1,min=0),
+            numericInput("WEIGHT", "Please input your weight(lbs):", value = 170,
+                     step = 0.25,min=0),
+            helpText("Note: Our result is based on the data of males from age 22-81.
+                     If you are female and the age is out of the range, 
+                     the result might not be accurate"),
+            actionButton("calculate", "Let's calculate!"),
+            actionButton("mail","Any problem? Contact us!")
         ),
-        
-        
+
+
         mainPanel(
-            textOutput("BodyFat")
+           h4("Your body fat is:"),
+           textOutput("BodyFat"),
+           textOutput("email")
         )
     )
 )
@@ -37,21 +42,35 @@ ui <- fluidPage(theme = "cerulean",
 
 
 server <- function(input, output) {
-    
-    BodyFat = reactive({
+
+    BodyFat = eventReactive(input$calculate,{
         BodyFat = -42.95790+0.90152*input$ABDOMEN-0.11994*input$WEIGHT
         return(BodyFat)
     })
     
     output$BodyFat <- renderText({
         if( BodyFat()>0 & BodyFat()<100){
-            paste("Your body fat is ",round(BodyFat(),2),"%.",sep="")
+            paste(round(BodyFat(),2),"%",sep="")
         }else{
             paste("Ops! There might be something wrong with your data. Please try again.")
         }
     })
     
+    contact = eventReactive(input$mail,{
+        return (1)
+    })
+    
+    output$email <- renderText({
+        if (contact()==1){
+            paste("Please email your problem to our email address: hpan55@wisc.edu")
+        }
+    })
+    
+    
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+
+
